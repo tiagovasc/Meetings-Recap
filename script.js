40149b3950c35af59d349efda0a2a6a1f9a6bf60
Tiago <39215546+tiagovasc@users.noreply.github.com>
2023-06-27 22:12:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,29 @@ window.onload = function() {
     document.getElementById("runBtn").onclick = function() {
         var transcript = document.getElementById("transcript").value.split('\n');
         var summary = document.getElementById("summary").value.split('\n');
-        
+
         var outputArea = document.getElementById("outputArea");
         outputArea.innerHTML = ""; // Clear previous results
 
         var transcriptIndex = 0;
         for (var i = 0; i < summary.length; i++) {
             var timestamps = summary[i].match(/\d+:\d+ - \d+:\d+/g)[0].split(' - ');
-            var startTimestamp = timestamps[0];
-            var endTimestamp = timestamps[1];
+            var startTimestampSeconds = convertTimestampToSeconds(timestamps[0]);
+            var endTimestampSeconds = convertTimestampToSeconds(timestamps[1]);
 
             var transcriptSection = '';
-            while (transcriptIndex < transcript.length && !transcript[transcriptIndex].includes(endTimestamp)) {
-                if (transcript[transcriptIndex].includes(startTimestamp)) {
-                    // Start collecting the lines for this section
-                    transcriptSection += transcript[transcriptIndex] + '\n';
-                }
+            while (transcriptIndex < transcript.length && convertTimestampToSeconds(getTimestampFromLine(transcript[transcriptIndex])) <= endTimestampSeconds) {
+                transcriptSection += transcript[transcriptIndex] + '\n';
                 transcriptIndex++;
             }
-            // Add the final line for this section
-            transcriptSection += transcript[transcriptIndex] + '\n';
-            transcriptIndex++;  // Move to the next line in the transcript
-            
+
             // Create new textarea for the section
             var textarea = document.createElement("textarea");
             textarea.rows = 10;
             textarea.cols = 100;
             textarea.value = transcriptSection;
             outputArea.appendChild(textarea);
-            
+
             // Create new copy button for the section
             var button = document.createElement("button");
             button.innerHTML = "Copy";
@@ -41,4 +35,14 @@ window.onload = function() {
             outputArea.appendChild(button);
         }
     }
+}
+
+function convertTimestampToSeconds(timestamp) {
+    var parts = timestamp.split(':');
+    return (+parts[0]) * 60 * 60 + (+parts[1]) * 60;
+}
+
+function getTimestampFromLine(line) {
+    var parts = line.split(' ');
+    return parts[1];
 }
