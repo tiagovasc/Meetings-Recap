72c0417759727a6303b7eb63d388bc0e0be33ea2
Tiago <39215546+tiagovasc@users.noreply.github.com>
2023-06-27 22:09:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 window.onload = function() {
     document.getElementById("runBtn").onclick = function() {
-        var transcript = document.getElementById("transcript").value;
-        var summary = document.getElementById("summary").value;
-        var sections = summary.split('\n');
+        var transcript = document.getElementById("transcript").value.split('\n');
+        var summary = document.getElementById("summary").value.split('\n');
         
         var outputArea = document.getElementById("outputArea");
         outputArea.innerHTML = ""; // Clear previous results
 
-        sections.forEach(section => {
-            var timestamps = section.match(/\d+:\d+ - \d+:\d+/g)[0].split(' - ');
+        var transcriptIndex = 0;
+        for (var i = 0; i < summary.length; i++) {
+            var timestamps = summary[i].match(/\d+:\d+ - \d+:\d+/g)[0].split(' - ');
             var startTimestamp = timestamps[0];
             var endTimestamp = timestamps[1];
-            var transcriptSection = transcript.substring(transcript.indexOf(startTimestamp), transcript.indexOf(endTimestamp));
+
+            var transcriptSection = '';
+            while (transcriptIndex < transcript.length && !transcript[transcriptIndex].includes(endTimestamp)) {
+                if (transcript[transcriptIndex].includes(startTimestamp)) {
+                    // Start collecting the lines for this section
+                    transcriptSection += transcript[transcriptIndex] + '\n';
+                }
+                transcriptIndex++;
+            }
+            // Add the final line for this section
+            transcriptSection += transcript[transcriptIndex] + '\n';
+            transcriptIndex++;  // Move to the next line in the transcript
             
             // Create new textarea for the section
             var textarea = document.createElement("textarea");
@@ -28,6 +39,6 @@ window.onload = function() {
                 document.execCommand("copy");
             }
             outputArea.appendChild(button);
-        });
+        }
     }
 }
